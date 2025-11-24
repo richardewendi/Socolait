2c88af513051bc9a24431475f7caab5815eb46d6
richardewendi <richardewendi@gmail.com>
2025-11-24 16:16:54 +0100

    Update db.js

--- api-server/db.js
+++ api-server/db.js
@@ -1,11 +1,12 @@
+// db.js
 import pkg from "pg";
 const { Pool } = pkg;
 
 export const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    host: process.env.DB_HOST,
+    user: process.env.DB_USER,        // utilisateur DB Render
+    password: process.env.DB_PASS,    // mot de passe DB Render
+    host: process.env.DB_HOST,        // host DB Render
     port: process.env.DB_PORT || 5432,
-    database: process.env.DB_NAME,
+    database: process.env.DB_NAME,    // nom de la DB
     ssl: { rejectUnauthorized: false } // utile si DB distante avec SSL
 });
