3c3b22deee230de6514d46608187aaaf30dda4be
richardewendi <richardewendi@gmail.com>
2025-11-25 12:08:27 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  // 💥 AJOUTEZ CETTE LIGNE POUR LES CHEMINS RELATIFS 💥
+  base: './', 
   server: {
     proxy: {
       "/api": {
